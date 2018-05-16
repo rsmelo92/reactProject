3df1d60cf492c7d629c3034815f5b95f16470191
Rafael Melo <rsmelo_@hotmail.com>
2018-05-16 20:40:11 -0300

    Faker implemented

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,11 +1,53 @@
 import React, { Component } from 'react';
-// import ReactDataGrid from 'react-data-grid';
+import ReactDataGrid from 'react-data-grid';
+import faker from 'faker';
 
 class Grid extends Component {
+    constructor() {
+        super();
+        this.createRows();
+        this._columns = [
+            { key: 'id', name: 'ID' },
+            { key: 'avatar', name: 'Avatar' },
+            { key: 'username', name: 'Username' },
+            { key: 'first', name: 'First Name' },
+            { key: 'last', name: 'Last Name' },
+            { key: 'email', name: 'Email' },
+        ];
+    
+        this.state = null;
+    }
+    
+    createRows = () => {
+        let rows = [];
+        for (let i = 1; i < 10; i++) {
+            rows.push({
+                id:i,
+                avatar:faker.internet.avatar(),
+                username:faker.internet.userName(),
+                first:faker.name.firstName(),
+                last:faker.name.lastName(),
+                email:faker.internet.email(),
+            });
+        }
+
+        this._rows = rows;
+    };
+
+    rowGetter = (i) => {
+        return this._rows[i];
+    };
+
     render(){
         return (
             <section className="grid-holder">
-                <h2>Grid</h2>
+                <ReactDataGrid
+                    columns={this._columns}
+                    rowGetter={this.rowGetter}
+                    rowsCount={this._rows.length}
+                    minHeight={500} 
+                />
+                
             </section>
         )
     }
---
+++ src/components/Grid/Grid2.js
@@ -0,0 +1,56 @@
+// import React, { Component } from 'react';
+// import ReactDataGrid from 'react-data-grid';
+// import faker from 'faker';
+
+// class Grid2 extends Component {
+//     constructor() {
+//         super();
+//         this.createRows();
+//         this._columns = [
+//             { key: 'id', name: 'ID' },
+//             { key: 'avatar', name: 'Avatar' },
+//             { key: 'username', name: 'Username' },
+//             { key: 'first', name: 'First Name' },
+//             { key: 'last', name: 'Last Name' },
+//             { key: 'email', name: 'Email' },
+//         ];
+    
+//         this.state = null;
+//     }
+    
+//     createRows = () => {
+//         let rows = [];
+//         for (let i = 1; i < 10; i++) {
+//             rows.push({
+//                 id:i,
+//                 avatar:faker.internet.avatar(),
+//                 username:faker.internet.userName(),
+//                 first:faker.name.firstName(),
+//                 last:faker.name.lastName(),
+//                 email:faker.internet.email(),
+//             });
+//         }
+
+//         this._rows = rows;
+//     };
+
+//     rowGetter = (i) => {
+//         return this._rows[i];
+//     };
+
+//     render(){
+//         return (
+//             <section className="grid-holder">
+//                 <ReactDataGrid
+//                     columns={this._columns}
+//                     rowGetter={this.rowGetter}
+//                     rowsCount={this._rows.length}
+//                     minHeight={500} 
+//                 />
+                
+//             </section>
+//         )
+//     }
+// }
+
+// export default Grid2;
