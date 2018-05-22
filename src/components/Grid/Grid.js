6e60ffdd6ea5e3e905117907a06d5707ca908389
Rafael Melo <rsmelo_@hotmail.com>
2018-05-22 16:02:31 -0300

    Starting to get the grid addons

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,34 +1,76 @@
-import React, { Component } from 'react';
-import ReactDataGrid from 'react-data-grid';
+// import update from 'immutability-helper';
 import faker from 'faker';
+import ReactDataGrid from 'react-data-grid';
+import React, { Component } from 'react';
+import { 
+        // Editors,
+        //  Toolbar, 
+         Formatters 
+        } from 'react-data-grid-addons';
+// const { AutoComplete: AutoCompleteEditor, DropDownEditor } = Editors; 
+const { ImageFormatter } = Formatters;
 
 class Grid extends Component {
-    constructor() {
-        super();
-        this.createRows();
+    constructor(props, context) {
+        super(props, context);
+        // this.createRows();
         this._columns = [
-            { key: 'id', name: 'ID' },
-            { key: 'avatar', name: 'Avatar' },
-            { key: 'username', name: 'Username' },
-            { key: 'first', name: 'First Name' },
-            { key: 'last', name: 'Last Name' },
-            { key: 'email', name: 'Email' },
+            { 
+                key: 'id', 
+                name: 'ID', 
+                width: 60,  
+                sortable:true,              
+            },
+            { 
+                key: 'avatar', 
+                name: 'Avatar',
+                width: 60,
+                formatter: ImageFormatter,
+                resizable: true,
+            },
+            { 
+                key: 'username',
+                name: 'Username',
+                width: 200,  
+                sortable:true,                           
+            },
+            { 
+                key: 'first',
+                name: 'First Name',
+                width: 200,                
+                sortable:true,
+            },
+            { 
+                key: 'last',
+                name: 'Last Name', 
+                width: 200,             
+                sortable:true,
+            },
+            { 
+                key: 'email',
+                name: 'Email', 
+                width: 250,
+                sortable:true,                              
+            },
         ];
-    
-        this.state = null;
+
+        // let originalRows = this.createRows();
+        // console.log("rows", originalRows)
+        // let rows = originalRows.slice(0);
+        // this.state = { originalRows, rows };
     }
-    
+
     createRows = () => {
         let rows = [];
         for (let i = 1; i <= 10; i++) {
             rows.push({
-                id:i,
-                avatar:faker.internet.avatar(),
-                username:faker.internet.userName(),
-                first:faker.name.firstName(),
-                last:faker.name.lastName(),
-                email:faker.internet.email(),
-            });
+                        id:i,
+                        avatar:faker.internet.avatar(),
+                        username:faker.internet.userName(),
+                        first:faker.name.firstName(),
+                        last:faker.name.lastName(),
+                        email:faker.internet.email(),
+                    });
         }
 
         this._rows = rows;
@@ -37,19 +79,31 @@ class Grid extends Component {
     rowGetter = (i) => {
         return this._rows[i];
     };
-
-    render(){
-        return (
-            <section className="grid-holder">
-                <ReactDataGrid
-                    columns={this._columns}
-                    rowGetter={this.rowGetter}
-                    rowsCount={this._rows.length}
-                    minHeight={500} 
-                />
-                
-            </section>
-        )
+    handleGridSort = (sortColumn, sortDirection) => {
+        console.log("sortColumn", sortColumn);
+        console.log("sortDirection", sortDirection);
+        const comparer = (a, b) => {
+            if (sortDirection === 'ASC') {
+                return (a[sortColumn] > b[sortColumn]) ? 1 : -1;
+            } else if (sortDirection === 'DESC') {
+                return (a[sortColumn] < b[sortColumn]) ? 1 : -1;
+            }
+        };
+        // console.log("this.state.originalRows.slice(0)",this.state.originalRows.slice(0))
+        // const rows = (sortDirection === 'NONE') ? this.state.originalRows.slice(0) : this.state.rows.sort(comparer);
+    
+        // this.setState({ rows });
+      };
+    render() {
+        return  (
+            <ReactDataGrid
+                onGridSort={this.handleGridSort}
+                columns={this._columns}
+                rowGetter={this.rowGetter}
+                rowsCount={this._rows.length}
+                minHeight={500} 
+            />
+        );
     }
 }
 
--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -2,16 +2,17 @@ import React, { Component } from 'react';
 import FixedNav from '../../components/FixedNav/FixedNav.js';
 import Grid from '../../components/Grid/Grid.js';
 
-
 class Dashboard extends Component {
 
     render(){
+
         return (
             <div>
                 <FixedNav />
                 <Grid />
             </div>
         )
+
     }
 }
 
